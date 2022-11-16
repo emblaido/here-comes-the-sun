92d3ae79bdfa592d9d1757c488930aa6849dafb8
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-11-16 11:01:23 -0600

    getting going with some info

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("welcome")
+
+
+
+
+
+
+let winningElement = document.getElementById("winningscore")
+console.log(winningElement)
+
+let loosingElemt = document.getElementById("loosingscore")
+console.log(loosingElemt)
