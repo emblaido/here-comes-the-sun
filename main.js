d55c20d53bd4df08e2a0f764aabb1d0cdcc08e59
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-11-16 11:18:37 -0600

    OH MY GOD ITS WORKIN

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 console.log("welcome")
 //getting started, adding all of my ids into the global so i can access them.
 //these are my buttons
-let startBtnElement = document.getElementById("startBtn")
-console.log(startBtnElement)
+let startBtnElement = document.getElementById("startBtn").addEventListener("click", function() {
+alert("lets get started!");
+console.log(startBtnElement);
+});
 
-let restartBtnElement = document.getElementById("restartBtn")
-console.log(restartBtnElement)
+let restartBtnElement = document.getElementById("restartBtn").addEventListener("click", function() {
+alert("dont be shy...try again!");
+console.log(restartBtnElement);
+});
+    
  
 //these are my score trackers 
 let winningElement = document.getElementById("winningscore")
