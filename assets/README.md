72b2a9b6dc4bca689a30d466ab2018a2665398bf
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-11-17 11:20:09 -0600

    just playing around

--- assets/README.md
+++ assets/README.md
@@ -1,1 +1,5 @@
 # here-comes-the-sun
+Here in the Garden, we like to play trivia games! ill pick something in the Garden
+        and ask you a question, if you get ten right answers, you win! 🌺🍃 
+        but if you get three wrong, you loose 🥀🍂  dont worry, nothing TOO bad happens if you loose....🔥
+        take a deep breath, and do your best! good luck.
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 console.log("welcome")
 //getting started, adding all of my ids into the global so i can access them.
 //these are my buttons.
-let startBtnElement = document.getElementById("startBtn");
-console.log(startBtnElement);
+let aButton = document.getElementById("optionA");
+console.log(aButton);
 
 
-let restartBtnElement = document.getElementById("restartBtn");
-console.log(restartBtnElement);
+let bButton = document.getElementById("optionB");
+console.log(bButton);
 
-    
+ let cButton = document.getElementById("optionC");
+ console.log(cButton)
  
 //these are my score trackers 
 let winningElement = document.getElementById("winningscore")
@@ -18,5 +19,7 @@ let loosingElemt = document.getElementById("loosingscore")
 console.log(loosingElemt)
 
 
-
-
+const myQuestions = [ "ginkgo", "snail", "lily"];
+for (let i = 0; i<myQuestions.length; i++) {
+    console.log([i])
+}
