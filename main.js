bfcefa8121510cab33e17916dd4035ed9bc83219
Emma Blaido <emmablaido@Emmas-Air.lan>
2022-11-18 10:59:02 -0600

    stuck but still pushing

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 console.log("welcome")
 
 //getting started, adding all of my ids into the global so i can access them.
+// these are my question id's that are hiddin and will be triggerd 
+const questionLily = document.getElementById("lily")
+questionLily.style.display = "none"
+
+const questionKinkgo = document.getElementById('Kinkgo')
+questionKinkgo.style.display = "none"
+
 //these are my questions, i am attempting to to make it orderly 
 
 let aButton = document.getElementById("optionA");
@@ -34,24 +41,34 @@ const questionsAry =  [
     {
 question1: "what are snails classified as?",
     options: ["option A", "option B", "option C"],
-right: "optionB"  
+right: "optionC"  
 },
  {
     question2: "is the lilly of the nile poisonous?",
     options: "optionA, optionB, optionC",
-    right: "optionC"
+    right: "optionB"
 },
 {
 
     question3: "what is the Ginkgo Biloba",
     options: "optionA, optionB, optionC",
-     right: "optionA, optionB, optionC",
+     right: "optionA, optionC",
 }
 ]
 //console.log(questions)
 
 updateButton(questionsAry[0])
 
+
+//setInterval(() => {
+    //questionLily = questionLily + 1
+//questionLily.innerHTMl = "lily" 
+//if (questionLily === 2) { 
+    //questionLily.style.display = "block"
+    
+//}    
+//}
+//)
 
 
 
